43505653e5a92736ce35c38c89424a456b69471f
Connie Leung <cleung334@gmail.com>
2024-01-09 23:14:22 +0800

    chore: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, assertInInjectionContext, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+
+function setTitle() {
+  assertInInjectionContext(setTitle);
+
+  const title = inject(Title);
+  title.setTitle('Ng Signal Input Demo');
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `test`,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'ng-signal-input-demo';
+  constructor() {
+    setTitle();
+  }
 }
